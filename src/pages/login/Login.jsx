fd8066c53b3c9aa9b07ed8d7b7b1fac41d729a95
ShreyaDayma-cse <shreyadayma0901@gmail.com>
2022-12-07 13:24:53 +0530

    changes

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -23,7 +23,7 @@ const Login = () =>{
         e.preventDefault()
         dispatch({type:"LOGIN_START"});
         try{
-            const res = await axios.post("https://backend-hotel-book.herokuapp.com/api/auth/login", credentials);
+            const res = await axios.post("https://shreyadayma-cse.github.io/backend/api/auth/login", credentials);
             dispatch({type:"LOGIN_SUCCESS", payload:res.data})
             navigate("/home")
            
